c88ec7c395a495c6df34bb8d4abfe1463bf56877
ARRT <r.arnaudin@kimo.fr>
2018-03-21 11:47:33 +0100

    - lib

--- src/index.js
+++
@@ -1,5 +0,0 @@
-// @flow
-export { waitForElement } from './waitForElement';
-export { waitForProps } from './waitForProps';
-export { waitForState } from './waitForState';
-export { wait } from './wait';
